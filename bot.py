bf2e14a16cd6adee282ce6d5aca41188d30f72fd
Evan Fox <iwc.evan@gmail.com>
2023-04-18 21:30:28 +0000

    Added Conversational history logic to bot

--- bot.py
+++ bot.py
@@ -4,30 +4,27 @@ import openai
 import discord
 from discord.ext import commands
 
-# Set up your OpenAI API key and Discord Bot Tokens
 try:
     openai.api_key = os.environ["OPENAI_API_KEY"]
     TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 except KeyError as e:
     raise ValueError("Missing environment variable: {}".format(str(e))) from None
 
-# Define the required intents
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Set up your bot
 bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
 
-# Rate limiting variables
 rate_limit_time = 5  # seconds
 rate_limit_dict = {}
+conversation_history = {}
 
 
-async def query_chatgpt(prompt):
+async def query_chatgpt(messages):
     try:
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": prompt}]
+            messages=messages
         )
     except openai.error.OpenAIError as e:
         raise ValueError("Error communicating with OpenAI API: {}".format(str(e))) from None
@@ -49,7 +46,8 @@ async def check_rate_limit(user_id):
 @bot.command(name="ask", help="Ask GPT-3.5 Turbo a question or send a message. To do this, use the !ask followed by your question or prompt.")
 async def ask(ctx, *, question):
     try:
-        time_remaining = await check_rate_limit(ctx.author.id)
+        user_id = ctx.author.id
+        time_remaining = await check_rate_limit(user_id)
         if time_remaining > 0:
             await ctx.send(f"{ctx.author.mention}, please wait {time_remaining:.1f} seconds before sending another request.")
             return
@@ -57,8 +55,17 @@ async def ask(ctx, *, question):
             await ctx.send(f"{ctx.author.mention}, error: Rate limiting failed. Please try again later.")
             return
 
-        prompt = f"{question.lower()}"
-        response = await query_chatgpt(prompt)
+        if user_id not in conversation_history:
+            conversation_history[user_id] = []
+
+        conversation_history[user_id].append({"role": "user", "content": question})
+        response = await query_chatgpt(conversation_history[user_id])
+        conversation_history[user_id].append({"role": "assistant", "content": response})
+
+        # Trim conversation history if it becomes too long
+        if len(conversation_history[user_id]) > 10:
+            conversation_history[user_id] = conversation_history[user_id][-10:]
+
         await ctx.send(f"{ctx.author.mention}, {response}")
     except discord.errors.HTTPException as e:
         raise ValueError("Error sending message to Discord: {}".format(str(e))) from None
